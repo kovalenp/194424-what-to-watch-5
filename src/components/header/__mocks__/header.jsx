c8905eb3fbee8cdd29c3e522dd8b5edfed1b1514
Pavel Kovalenko <pavel@kovalenko.cz>
2020-11-17 14:10:41 +0100

    Tried mock files

---
+++ src/components/header/__mocks__/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+
+  return (
+    <header className="page-header user-page__head">
+      <p>MockHeader</p>
+    </header>
+  );
+};
+
+export default Header;
