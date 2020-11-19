ec4e80bda42d46177c3e1a6e0c99d7351d935648
Pavel Kovalenko <pavel@kovalenko.cz>
2020-11-19 20:16:31 +0100

    Refactor add-new-comment into state component

--- src/components/add-new-comment/add-new-comment.jsx
+++ src/components/add-new-comment/add-new-comment.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
@@ -7,131 +7,121 @@ import {appRoute} from "../../common/constants";
 import browserHistory from "../../common/browser-history";
 import {sendReveiw} from "../../services/movie-service";
 
-class AddNewComment extends PureComponent {
-  constructor(props) {
-    super(props);
+const AddNewComment = (props) => {
 
-    this.state = {
-      rating: `3`,
-      reviewText: ``,
-      isSending: false,
-    };
+  const [state, setState] = useState({
+    rating: `3`,
+    reviewText: ``,
+    isSending: false,
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleInputChange = (e) => setState((prevState) => Object.assign({}, prevState, {[e.target.name]: e.target.value}));
 
-  handleInputChange(e) {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({isSending: true});
-    sendReveiw({id: this.props.id, rating: this.state.rating, comment: this.state.reviewText})
-      .then(() => this.setState({isSending: false, reviewText: ``}))
-      .then(() => this.props.getComments(this.props.id))
-      .then(() => browserHistory.push(appRoute.FILM.replace(`:id`, this.props.id)));
-  }
+    setState((prevState) => Object.assign({}, prevState, {isSending: true}));
+    sendReveiw({id: props.id, rating: state.rating, comment: state.reviewText})
+      .then(() => setState((prevState) => Object.assign({}, prevState, {isSending: false, reviewText: ``})))
+      .then(() => props.getComments(props.id))
+      .then(() => browserHistory.push(appRoute.FILM.replace(`:id`, props.id)));
+  };
 
-  render() {
-    return (
-      <div className="add-review">
-        <form
-          action="#"
-          className="add-review__form"
-          onSubmit={this.handleSubmit}
-        >
-          <div className="rating">
-            <div className="rating__stars">
-              <input
-                className="rating__input"
-                id="star-1"
-                type="radio"
-                name="rating"
-                value="1"
-                onChange={this.handleInputChange}
-                checked={this.state.rating === `1`}
-              />
-              <label className="rating__label" htmlFor="star-1">
-                Rating 1
-              </label>
+  return (
+    <div className="add-review">
+      <form
+        action="#"
+        className="add-review__form"
+        onSubmit={handleSubmit}
+      >
+        <div className="rating">
+          <div className="rating__stars">
+            <input
+              className="rating__input"
+              id="star-1"
+              type="radio"
+              name="rating"
+              value="1"
+              onChange={handleInputChange}
+              checked={state.rating === `1`}
+            />
+            <label className="rating__label" htmlFor="star-1">
+              Rating 1
+            </label>
 
-              <input
-                className="rating__input"
-                id="star-2"
-                type="radio"
-                name="rating"
-                value="2"
-                onChange={this.handleInputChange}
-                checked={this.state.rating === `2`}
-              />
-              <label className="rating__label" htmlFor="star-2">
-                Rating 2
-              </label>
+            <input
+              className="rating__input"
+              id="star-2"
+              type="radio"
+              name="rating"
+              value="2"
+              onChange={handleInputChange}
+              checked={state.rating === `2`}
+            />
+            <label className="rating__label" htmlFor="star-2">
+              Rating 2
+            </label>
 
-              <input
-                className="rating__input"
-                id="star-3"
-                type="radio"
-                name="rating"
-                value="3"
-                onChange={this.handleInputChange}
-                checked={this.state.rating === `3`}
-              />
-              <label className="rating__label" htmlFor="star-3">
-                Rating 3
-              </label>
+            <input
+              className="rating__input"
+              id="star-3"
+              type="radio"
+              name="rating"
+              value="3"
+              onChange={handleInputChange}
+              checked={state.rating === `3`}
+            />
+            <label className="rating__label" htmlFor="star-3">
+              Rating 3
+            </label>
 
-              <input
-                className="rating__input"
-                id="star-4"
-                type="radio"
-                name="rating"
-                value="4"
-                onChange={this.handleInputChange}
-                checked={this.state.rating === `4`}
-              />
-              <label className="rating__label" htmlFor="star-4">
-                Rating 4
-              </label>
+            <input
+              className="rating__input"
+              id="star-4"
+              type="radio"
+              name="rating"
+              value="4"
+              onChange={handleInputChange}
+              checked={state.rating === `4`}
+            />
+            <label className="rating__label" htmlFor="star-4">
+              Rating 4
+            </label>
 
-              <input
-                className="rating__input"
-                id="star-5"
-                type="radio"
-                name="rating"
-                value="5"
-                onChange={this.handleInputChange}
-                checked={this.state.rating === `5`}
-              />
-              <label className="rating__label" htmlFor="star-5">
-                Rating 5
-              </label>
-            </div>
+            <input
+              className="rating__input"
+              id="star-5"
+              type="radio"
+              name="rating"
+              value="5"
+              onChange={handleInputChange}
+              checked={state.rating === `5`}
+            />
+            <label className="rating__label" htmlFor="star-5">
+              Rating 5
+            </label>
           </div>
+        </div>
 
-          <div className="add-review__text"
-            style={{backgroundColor: `white`}}>
-            <textarea
-              className="add-review__textarea"
-              name="reviewText"
-              id="reviewText"
-              placeholder="Review text"
-              value={this.state.reviewText}
-              onChange={this.handleInputChange}
-            ></textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit" disabled={!(this.state.reviewText.length > 50) && !(this.state.isSending)}>
-                Post
-              </button>
-            </div>
+        <div className="add-review__text"
+          style={{backgroundColor: `white`}}>
+          <textarea
+            className="add-review__textarea"
+            name="reviewText"
+            id="reviewText"
+            placeholder="Review text"
+            value={state.reviewText}
+            onChange={handleInputChange}
+          ></textarea>
+          <div className="add-review__submit">
+            <button className="add-review__btn" type="submit" disabled={!(state.reviewText.length > 50) && !(state.isSending)}>
+              Post
+            </button>
           </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 AddNewComment.propTypes = {
   id: PropTypes.number.isRequired,
