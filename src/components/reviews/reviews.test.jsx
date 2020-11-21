27ead67725b8246f65ebc5d0e48df4b0b441e8fa
Pavel Kovalenko <pavel@kovalenko.cz>
2020-11-21 08:33:44 +0100

    Added mock formatDate call

--- src/components/reviews/reviews.test.jsx
+++ src/components/reviews/reviews.test.jsx
@@ -3,9 +3,14 @@ import renderer from "react-test-renderer";
 import Reviews from "./reviews";
 import reviews from "../../mocks/reviews";
 
+jest.mock(`../../utils/utils`, () => ({
+  __esModule: true,
+  formatDate: () => `September 3, 2020`,
+}));
+
 describe(`Reviews meets B21`, () => {
 
-  it(`and rendered correctly for auth'ed`, () => {
+  it(`and rendered correctly`, () => {
     const tree = renderer.create((
       <Reviews
         reviews={reviews} />
